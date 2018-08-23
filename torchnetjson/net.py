6cbc9aefd3e4ce999552f8304c41035ae3974efd
zym1010 <zym1010@gmail.com>
2018-08-22 20:45:42 -0400

    refactor code and set up general framework

---
+++ torchnetjson/net.py
@@ -0,0 +1,80 @@
+from torch import nn
+from .module import init_module
+
+
+class JSONNet(nn.Module):
+    def _add_modules(self, module_dict):
+        for attrname, (modname, modparams) in module_dict.items():
+            self.add_module(attrname, init_module(modname, modparams))
+
+    def get_module(self, name):
+        return self._modules[name]
+
+    def __init__(self, param_dict):
+        super().__init__()
+        self.__param_dict = param_dict
+        self._add_modules(param_dict['module_dict'])
+        # TODO some initialization stuff later.
+        # or I can do initialization in the module_dict directly.
+        # but that maybe a bit constrained.
+        # if you want to do some initialization that depends on multiple
+        # modules.
+        # I like to separate this from architecture spec.
+
+    def forward(self, *inputs, state_dict=None):
+        # state_dict is typically used for debugging,
+        # checking internal state, etc.
+        # it should be a subset of intermediate_dict
+        # I will basically do things according to op_list
+        temp_dict = {'inputs': inputs}
+        op_list = self.__param_dict['op_list']
+        for op_spec in op_list:
+            # this function does the actual work.
+            # it can be recursively called.
+            self._forward_one_op(op_spec, temp_dict)
+
+        return self.get_io(self.__param_dict['out'], temp_dict)
+
+    @staticmethod
+    def get_io(io_spec, temp_dict):
+        if isinstance(io_spec, str):
+            return temp_dict[io_spec]
+        elif isinstance(io_spec, list):
+            return [temp_dict[x] for x in io_spec]
+        else:
+            raise NotImplementedError
+
+    def _forward_one_op(self, op_spec, temp_dict):
+        from .op import get_op
+        # different kinds of ops.
+        #
+        # function op, concat, sequential, split, indexing, etc. they can take
+        #   additional parameters apart from op_in. also, we have detach.
+        #   for saving space
+        # module op, using defined modules. all they take is op_in
+        #
+        # op_name, op_params, op_in, op_out =
+        # ins can be a list, which contains a fixed number of inputs
+        # and it will be unpacked/packed when passed into ops,
+        # depending what you want.
+        #
+        # each op has its own packing/unpacking semantics
+        # and you can override when the input is a list when applicable,
+        # by passing additional parameters into the op.
+        #
+        # for example, torch.cat is packing.
+        #
+        # for sequential, I favor unpacking,
+        # which matches forward()'s API more closely.
+        # (which is violated by torch's own Sequential, which assumes
+        # single input).
+        #
+        # or a string, which is just a single input
+        #
+        # out will always be taken unchanged,
+        # which can be a list/tuple or singleton.
+        # splitting it is at the user's own disposal for now.
+        op_this = get_op(self, op_spec['name'],
+                         op_spec['args'], op_spec['kwargs'])
+        temp_dict[op_spec['out']] = op_this(self.get_io(op_spec['in'],
+                                                        temp_dict))
--- torchnetjson/op.py
+++ torchnetjson/op.py
@@ -15,4 +15,28 @@
 # i.e. for loop op, we should make sure it's intermediates has no
 # name collision with outer intermediate.
 # to achieve this, we should pass the same intermediate over and over
+import torch
+from .net import JSONNet
 
+
+def get_op(net: JSONNet, op_name: str,
+           args: list, kwargs: dict) -> function:
+    op_this = _op_dict[op_name](net, *args, **kwargs)
+    return op_this
+
+
+def _module_op(net: JSONNet, module_name: str, *, unpack=True) -> function:
+    # get module
+    def module_op_fn(inputs):
+        mod = net.get_module(module_name)
+        if isinstance(inputs, torch.Tensor) or not unpack:
+            return mod(inputs)
+        else:
+            return mod(*inputs)
+
+    return module_op_fn
+
+
+_op_dict = {
+    'module': _module_op,
+}
