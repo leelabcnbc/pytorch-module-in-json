f232a43a177e4d5395e5678f90ba026acfb21926
zym1010 <zym1010@gmail.com>
2018-08-27 09:32:14 -0400

    add more assert for version checks

---
+++ torchnetjson/__init__.py
@@ -0,0 +1,8 @@
+from sys import version_info
+import torch
+
+assert version_info >= (3, 6)
+# TODO: should be relaxed later.
+# ideally, we should test against each version in _tested_torch_ver
+_tested_torch_ver = {'0.4.1', }
+assert torch.__version__ in _tested_torch_ver
