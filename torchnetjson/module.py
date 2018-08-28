1397811fad4b50f0477309979ecc56cd6b60197f
zym1010 <zym1010@gmail.com>
2018-08-28 08:41:07 -0400

    add batchnorm2d

--- torchnetjson/module.py
+++ torchnetjson/module.py
@@ -20,6 +20,7 @@ def _register_module_official_loader() -> None:
     _register_module_official('torch.nn.relu', nn.ReLU)
     _register_module_official('torch.nn.softplus', nn.Softplus)
     _register_module_official('torch.nn.linear', nn.Linear)
+    _register_module_official('torch.nn.batchnorm2d', nn.BatchNorm2d)
 
 
 _register_module_official_loader()
