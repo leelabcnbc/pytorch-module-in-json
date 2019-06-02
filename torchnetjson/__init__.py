1bd02d401f7d23845e2db9bf2d643169d5d70a4a
Yimeng Zhang <zym1010@gmail.com>
2019-06-02 12:22:39 -0400

    add torch 1.1.0 support

--- torchnetjson/__init__.py
+++ torchnetjson/__init__.py
@@ -4,5 +4,5 @@ import torch
 assert version_info >= (3, 6)
 # TODO: should be relaxed later.
 # ideally, we should test against each version in _tested_torch_ver
-_tested_torch_ver = {'0.4.1', '1.0.0', '1.0.1'}
+_tested_torch_ver = {'0.4.1', '1.0.0', '1.0.1', '1.1.0'}
 assert torch.__version__ in _tested_torch_ver
